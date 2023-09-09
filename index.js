2994954aba2a530f0809c07465a4329cfe3e2339
Sharif Aly <sharif.aly_@outlook.com>
2023-09-09 09:59:33 +0200

    deleted all, and connected to database

--- index.js
+++ index.js
@@ -1,57 +1,27 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-import mysql from "mysql";
+import mysql from "mysql2";
 
 const app = express();
 
 const db = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "",
-  database: "react_bibliotheke",
+  password: "sally",
+  database: "bibliotheke",
 });
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const port = 4000;
+const port = 3001;
 
-app.get("/book/all", (req, res) => {
+app.get("/", (req, res) => {
   const sqlSelect = "SELECT * FROM `books`";
 
   db.query(sqlSelect, (err, result) => {
-    res.send(result);
-  });
-});
-
-app.post("/book/insert", (req, res) => {
-  const Title = req.body.Title;
-  const Description = req.body.Description;
-  const Cover = req.body.Cover;
-  const Price = req.body.Price;
-
-  const sqlInsert =
-    "INSERT INTO `books`(`Title`, `Description`, `Cover`, `Price`) VALUES (?, ?, ?, ?)";
-
-  db.query(sqlInsert, [Title, Description, Cover, Price], (err, result) => {
     console.log(result);
-  });
-});
-
-app.delete("/book/delete/:id", (req, res) => {
-  const id = req.params.id;
-
-  const sqlDelete = "DELETE FROM `books` WHERE Book_ID = ?";
-
-  db.query(sqlDelete, [id], (err, result) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send("Error deleting book");
-    } else {
-      console.log(result);
-      res.send("Book deleted successfully");
-    }
   });
 });
 
