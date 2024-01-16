9913c5614d0a88c3466697edda50d742ebdd16bc
Sharif Aly <sharif.aly_@outlook.com>
2024-01-16 20:02:22 +0100

    fix: fixed the post request

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ import cors from "cors";
 
 const app = express();
 app.use(cors());
+app.use(express.json()); // Add this line to parse request body
 const port = 8000;
 
-// Create a MySQL connection
-const connection = mysql.createConnection({
+// Create MySQL connection
+const db = mysql.createPool({
   host: "localhost",
   user: "root",
   password: "",
@@ -15,19 +16,19 @@ const connection = mysql.createConnection({
 });
 
 // Connect to the database
-connection.connect((error) => {
-  if (error) {
-    console.error("Error connecting to the database:", error);
-  } else {
-    console.log("Connected to the database");
-  }
-});
+// connection.connect((error) => {
+//   if (error) {
+//     console.error("Error connecting to the database:", error);
+//   } else {
+//     console.log("Connected to the database");
+//   }
+// });
 
-// Define your routes and middleware here
+// Routes and Middleware
 
 // Route to print all data from "books"
 app.get("/books", (req, res) => {
-  connection.query("SELECT * FROM books", (error, results) => {
+  db.query("SELECT * FROM books", (error, results) => {
     if (error) {
       console.error("Error querying the database:", error);
       res.status(500).send("Error querying the database");
@@ -36,6 +37,27 @@ app.get("/books", (req, res) => {
       res.json(results);
     }
   });
+});
+
+// Route to create a new book
+
+// Fix the post request
+
+app.post("/books", (req, res) => {
+  // const upload = multer({ dest: "uploads/" });
+  const { Title, Description, Cover } = req.body; // Use req.body to access the data that was sent in the post request
+  db.query(
+    "INSERT INTO `books`(`Title`, `Description`, `Cover`) VALUES (?, ?, ?)", // Update the column names in the query
+    [Title, Description, Cover], // Use the correct variable names
+    (error, results) => {
+      if (error) {
+        console.error("Error querying the database:", error);
+        res.status(500).send("Error querying the database");
+      } else {
+        res.json(results);
+      }
+    }
+  );
 });
 
 // Start the server
